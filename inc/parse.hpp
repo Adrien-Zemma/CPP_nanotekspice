c6c72d618955c6c97cb993dcc2659d21d3fbc0dc
Hugo MARTIN <hugo.martin@epitech.eu>
2018-02-08 10:29:29 +0000

    Bug with parsing class

--- inc/parse.hpp
+++ inc/parse.hpp
@@ -13,8 +13,8 @@
 #include <map>
 #include <fstream>
 #include <iterator>
-#include <vector>
 #include <sstream>
+#include <vector>
 
 class Parse
 {
@@ -25,12 +25,22 @@ public:
 	
 	void read();
 	void read(std::string file);
-	std::map<std::string, std::string> *getData() const;
 	std::vector<std::string> split_line(const std::string &line) const;
 
+	enum ParseTitle {
+		CHIPSETS,
+		LINKS
+	};
+
 private:
-	std::map<std::string, std::string>	*_data;
+	int count_args(const std::string line) const;
+	void append_line(std::string line);
+	void dumpChipsets() const;
+
+	std::map<std::string, std::string>		*_chipsets;
+	std::map<std::pair<std::string, size_t>, std::pair<std::string, size_t>>	*_links;
 	std::string				_file;
+	Parse::ParseTitle			_title;
 };
 
 #endif /* !PARSE_HPP_ */
--- src/parsing/parse.cpp
+++ src/parsing/parse.cpp
@@ -9,18 +9,21 @@
 Parse::Parse()
 	: _file("")
 {
-	_data = new std::map<std::string, std::string>;
+	_links = new std::map<std::pair<std::string, size_t>, std::pair<std::string, size_t>>;
+	_chipsets = new std::map<std::string, std::string>;
 }
 
 Parse::Parse(const std::string &file)
 	: _file(file)
 {
-	_data = new std::map<std::string, std::string>;
+	_links = new std::map<std::pair<std::string, size_t>, std::pair<std::string, size_t>>;
+	_chipsets = new std::map<std::string, std::string>;
 }
 
 Parse::~Parse()
 {
-	delete _data;
+	delete _links;
+	delete _chipsets;
 }
 
 void Parse::read()
@@ -41,18 +44,41 @@ void Parse::read(std::string file)
 		return ;
 	}
 	std::string line;
-	while (getline(in, line))
-	{
-		std::vector<std::string> elems = split_line(line);
-		if (elems.size() == 2)
-			_data->insert(std::pair<std::string, std::string>(elems[0], elems[1]));
-		else if (elems.size() == 1)
-		_data->insert(std::pair<std::string, std::string>(elems[0], ""));
+	while (getline(in, line)) {
+		if (line.rfind(".chipsets:", 0) == 0)
+			_title = CHIPSETS;
+		else if (line.rfind(".links:", 0) == 0)
+			_title = LINKS;
+		append_line(line);
 	}
-	std::cout << "Dump _data\n";
-	for (auto &it : *_data)
-    		std::cout << it.first << " => " << it.second << '\n';
-	
+	dumpChipsets();
+}
+
+void Parse::append_line(std::string line)
+{
+	auto elems = split_line(line);
+	int args = count_args(line);
+
+	switch(_title) {
+		case CHIPSETS:
+		if (elems.size() == 2)
+			_chipsets->insert(std::pair<std::string,std::string>(elems[0], elems[1]));
+		case LINKS:
+		break;
+	}
+}
+
+int Parse::count_args(const std::string line) const
+{
+	size_t	nb = 0;
+
+	for (auto &ch : line) {
+		if (ch == ' ' || ch == '\t')
+			nb++;
+		else if (ch == '#')
+			return -1;
+	}
+	return (nb == 0 ? 1 : nb);
 }
 
 std::vector<std::string> Parse::split_line(const std::string &line) const
@@ -60,7 +86,7 @@ std::vector<std::string> Parse::split_line(const std::string &line) const
 	std::vector<std::string> elems;
 	std::string el;
 
-	for (unsigned int i = 0; i < line.length(); i++) {
+	for (size_t i = 0; i < line.length(); i++) {
 		if (line[i] == '#') {
 			if (!el.empty())
 				elems.push_back(el);
@@ -79,7 +105,9 @@ std::vector<std::string> Parse::split_line(const std::string &line) const
 	return elems;
 }
 
-std::map<std::string, std::string> *Parse::getData() const
+void Parse::dumpChipsets() const
 {
-	return _data;
+	for (const auto &it : _chipsets) {
+		std::cout << it.first << " => " << it.second << std::endl;
+	}
 }
